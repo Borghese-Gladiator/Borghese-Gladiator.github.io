4951d05bb462e25cf6d2d2df24b0b917daad213e
Timothy Shee <timothy_shee@hotmail.com>
2020-10-10 23:21:16 -0400

    Updates

---
+++ precache-manifest.b2bde1831074241002aae2fcedce6fc6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589635f2cfdbd022480ae7bcbf98562b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "746893c95711465d6d35",
+    "url": "/static/css/2.ab965100.chunk.css"
+  },
+  {
+    "revision": "6ddde6d3820c182da05a",
+    "url": "/static/css/main.033b9120.chunk.css"
+  },
+  {
+    "revision": "746893c95711465d6d35",
+    "url": "/static/js/2.30437087.chunk.js"
+  },
+  {
+    "revision": "1f4e5274478b14df53f481895eac4de6",
+    "url": "/static/js/2.30437087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ddde6d3820c182da05a",
+    "url": "/static/js/main.8f06734f.chunk.js"
+  },
+  {
+    "revision": "ecab03fac402545509f1",
+    "url": "/static/js/runtime-main.ecb66fc9.js"
+  },
+  {
+    "revision": "74871842a78648e265538f2752e22368",
+    "url": "/static/media/Audio-Waveforms-Featued-Image.74871842.jpg"
+  },
+  {
+    "revision": "1ad5427100d5576690f4f0211fcecad2",
+    "url": "/static/media/Can-data-science-predict-the-stock-market-.1ad54271.jpg"
+  },
+  {
+    "revision": "ef7714669b4a7d200794d57f6b0c7929",
+    "url": "/static/media/Current_Resume.ef771466.pdf"
+  },
+  {
+    "revision": "3557b79181f0473f7c28bc288b63b9b9",
+    "url": "/static/media/HackUMass-Seven-Logo.3557b791.png"
+  },
+  {
+    "revision": "dbbaf5c703e0378ca40b3fab3ed2707e",
+    "url": "/static/media/HackUMass-Six-Logo-1.dbbaf5c7.png"
+  },
+  {
+    "revision": "cad61d0547875641a3ee75429b93668e",
+    "url": "/static/media/Lexhack-Logo.cad61d05.jpg"
+  },
+  {
+    "revision": "f0abe0aed2b6f129f6293a02f7941d4b",
+    "url": "/static/media/People-Collage.f0abe0ae.jpg"
+  },
+  {
+    "revision": "17693b0767f3887a1c6833d7077cd454",
+    "url": "/static/media/Precious.17693b07.ttf"
+  },
+  {
+    "revision": "3a4ebe70a8af6665cd18aee5302d95b5",
+    "url": "/static/media/Precious.3a4ebe70.eot"
+  },
+  {
+    "revision": "974e10be1d9dd31e956a1e30bc71df1c",
+    "url": "/static/media/Precious.974e10be.svg"
+  },
+  {
+    "revision": "a7929d7774aa6513328a15cde0992953",
+    "url": "/static/media/Precious.a7929d77.woff2"
+  },
+  {
+    "revision": "eb15ee2de9e30c6897c7bf96e495f21d",
+    "url": "/static/media/Precious.eb15ee2d.woff"
+  },
+  {
+    "revision": "bd948c3028be74b2c9a9974a806c2930",
+    "url": "/static/media/RSA_Security_Logo.bd948c30.png"
+  },
+  {
+    "revision": "ba9f6ee27339ab39638c95eff8fb6130",
+    "url": "/static/media/community-hero.ba9f6ee2.png"
+  },
+  {
+    "revision": "3942d6ef5b2e0d3732082bd9b28c853f",
+    "url": "/static/media/intro-bg.3942d6ef.png"
+  },
+  {
+    "revision": "ccbb75272f5aee66be9b8517c1e65ce1",
+    "url": "/static/media/kitten-puppy-cute.ccbb7527.png"
+  },
+  {
+    "revision": "d2adc0d15869d3a5d9826021c6aaac14",
+    "url": "/static/media/leap-motion.d2adc0d1.png"
+  },
+  {
+    "revision": "406466fbdcbaf37a06726442d3f983cb",
+    "url": "/static/media/umass.406466fb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Borghese-Gladiator.github.io/precache-manifest.6c324fb89bc33057573f9c3ed71658b8.js"
+  "/precache-manifest.b2bde1831074241002aae2fcedce6fc6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Borghese-Gladiator.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
