bf6b225dc2024dc05ed4f76dc8872a0456172000
Timothy Shee <timothy_shee@hotmail.com>
2020-11-18 01:43:26 -0500

    Remove unused Link import

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -6,7 +6,6 @@ import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import Link from '@material-ui/core/Link'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
 
 import CardActionArea from '@material-ui/core/CardActionArea';
